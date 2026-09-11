9155db5dad87dbc1e6b18e64f192709a6a38897b
Mariam Omara <mariamomara2005@gmail.com>
2026-09-12 01:09:42 +0300

    Q3

--- C#ASS-05/Program.cs
+++ C#ASS-05/Program.cs
@@ -1,62 +1,70 @@
-﻿//namespace C_ASS_05
+﻿
+
+//namespace C_ASS_05
 //{
 //    internal class Program
 //    {
 
 
-       
-
-//            //Q2
-//            static void Main(string[] args)
-//            {
-//                PrintBookInfo("Clean Code");
-
-//                PrintBookInfo("C# Basics", 400);
-//            }
+//        //            //Q2
+//        static void Main(string[] args)
+//{
+//    //Q3
+//    PrintBookInfo(pages: 500, title: "Clean Code");
+//}
 
 
 
 
 
+    //                PrintBookInfo("Clean Code");
 
-
-            //            double price;
-
-            //            if (TryGetPrice("Clean Code", out price))
-            //            {
-            //                Console.WriteLine(price);
-            //            }
-            //        }
+    //                PrintBookInfo("C# Basics", 400);
+    //            }
 
 
 
 
-            //        // Q1
-            //        public static bool TryGetPrice(string title, out double price)
-            //        {
-            //            if (title == "Clean Code")
-            //            {
-            //                price = 25.5;
-            //                return true;
-            //            }
-            //            else
-            //            {
-            //                price = 0;
-            //                return false;
-            //            }
-            //        }
-            //    }
-            //}
 
 
-            // Q2
 
-    //        static void PrintBookInfo(string title, int pages = 300)
+    //            double price;
+
+    //            if (TryGetPrice("Clean Code", out price))
+    //            {
+    //                Console.WriteLine(price);
+    //            }
+    //        }
+
+
+
+
+    //        // Q1
+    //        public static bool TryGetPrice(string title, out double price)
     //        {
-    //            Console.WriteLine(title);
-    //            Console.WriteLine(pages);
+    //            if (title == "Clean Code")
+    //            {
+    //                price = 25.5;
+    //                return true;
+    //            }
+    //            else
+    //            {
+    //                price = 0;
+    //                return false;
+    //            }
     //        }
     //    }
     //}
+
+
+    // Q2
+    //Q3
+//    static void PrintBookInfo(string title, int pages = 300)
+//{
+//    Console.WriteLine(title);
+//    Console.WriteLine(pages);
+//}
+//        }
+//    }
 
               
