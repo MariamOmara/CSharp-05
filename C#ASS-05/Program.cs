e3e98a1256c35dbe77b90e82a5a549063bbfdcdc
Mariam Omara <mariamomara2005@gmail.com>
2026-09-12 01:17:03 +0300

    Q6

--- C#ASS-05/Program.cs
+++ C#ASS-05/Program.cs
@@ -7,104 +7,122 @@
 //        //            //Q2
 //        static void Main(string[] args)
 //        {
-//            //Q5
 
 //            Book book = new Book();
 
-//            Console.WriteLine(book.Password);
+//            Console.WriteLine(book.copiesInStock);
 //        }
-//    }
+
+
+
+
+        //            //Q5
+
+        //            Book book = new Book();
+
+        //            Console.WriteLine(book.Password);
+        //        }
+        //    }
 
 
 
 
 
 
-    //            //Q4
-    //            PrintAllTitles("Clean Code", "C# Basics", "The Pragmatic Programmer");
-    //        }
+        //            //Q4
+        //            PrintAllTitles("Clean Code", "C# Basics", "The Pragmatic Programmer");
+        //        }
 
 
 
 
-    //    //Q3
-    //    PrintBookInfo(pages: 500, title: "Clean Code");
-    //}
-
-
-
-
-
-    //                PrintBookInfo("Clean Code");
-
-    //                PrintBookInfo("C# Basics", 400);
-    //            }
+        //    //Q3
+        //    PrintBookInfo(pages: 500, title: "Clean Code");
+        //}
 
 
 
 
 
+        //                PrintBookInfo("Clean Code");
 
-
-    //            double price;
-
-    //            if (TryGetPrice("Clean Code", out price))
-    //            {
-    //                Console.WriteLine(price);
-    //            }
-    //        }
-
-
-
-
-    //        // Q1
-    //        public static bool TryGetPrice(string title, out double price)
-    //        {
-    //            if (title == "Clean Code")
-    //            {
-    //                price = 25.5;
-    //                return true;
-    //            }
-    //            else
-    //            {
-    //                price = 0;
-    //                return false;
-    //            }
-    //        }
-    //    }
-    //}
-
-
-    // Q2
-    //Q3
-    //    static void PrintBookInfo(string title, int pages = 300)
-    //{
-    //    Console.WriteLine(title);
-    //    Console.WriteLine(pages);
-    //}
-    //        }
-    //    }
+        //                PrintBookInfo("C# Basics", 400);
+        //            }
 
 
 
 
 
-    //Q4
-    //        static void PrintAllTitles(params string[] titles)
-    //        {
-    //            foreach (string title in titles)
-    //            {
-    //                Console.WriteLine(title);
-    //            }
-    //        }
-    //    }
 
-    //}
 
-    //Q5
-//    class Book
-//    {
-//        private string password = "secret";
-//        public string Password => password;
+        //            double price;
+
+        //            if (TryGetPrice("Clean Code", out price))
+        //            {
+        //                Console.WriteLine(price);
+        //            }
+        //        }
+
+
+
+
+        //        // Q1
+        //        public static bool TryGetPrice(string title, out double price)
+        //        {
+        //            if (title == "Clean Code")
+        //            {
+        //                price = 25.5;
+        //                return true;
+        //            }
+        //            else
+        //            {
+        //                price = 0;
+        //                return false;
+        //            }
+        //        }
+        //    }
+        //}
+
+
+        // Q2
+        //Q3
+        //    static void PrintBookInfo(string title, int pages = 300)
+        //{
+        //    Console.WriteLine(title);
+        //    Console.WriteLine(pages);
+        //}
+        //        }
+        //    }
+
+
+
+
+
+        //Q4
+        //        static void PrintAllTitles(params string[] titles)
+        //        {
+        //            foreach (string title in titles)
+        //            {
+        //                Console.WriteLine(title);
+        //            }
+        //        }
+        //    }
+
+        //}
+
+        //Q5
+        //    class Book
+        //    {
+        //        private string password = "secret";
+        //        public string Password => password;
+        //    }
+        //}
+
+
+        //Q6
+//        class Book
+//        {
+//            internal int copiesInStock = 5;
+//        }
 //    }
 //}
