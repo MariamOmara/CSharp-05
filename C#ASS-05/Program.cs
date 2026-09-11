0d58bec372300c364eb43301fc260ed3dedaa5a7
Mariam Omara <mariamomara2005@gmail.com>
2026-09-12 01:14:41 +0300

    Q5

--- C#ASS-05/Program.cs
+++ C#ASS-05/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-//namespace C_ASS_05
+﻿//namespace C_ASS_05
 //{
 //    internal class Program
 //    {
@@ -9,92 +7,104 @@
 //        //            //Q2
 //        static void Main(string[] args)
 //        {
+//            //Q5
 
-//            //Q4
-//            PrintAllTitles("Clean Code", "C# Basics", "The Pragmatic Programmer");
-//        }
+//            Book book = new Book();
 
-
-
-
-
-
-
-
-
-
-
-
-        //    //Q3
-        //    PrintBookInfo(pages: 500, title: "Clean Code");
-        //}
-
-
-
-
-
-        //                PrintBookInfo("Clean Code");
-
-        //                PrintBookInfo("C# Basics", 400);
-        //            }
-
-
-
-
-
-
-
-        //            double price;
-
-        //            if (TryGetPrice("Clean Code", out price))
-        //            {
-        //                Console.WriteLine(price);
-        //            }
-        //        }
-
-
-
-
-        //        // Q1
-        //        public static bool TryGetPrice(string title, out double price)
-        //        {
-        //            if (title == "Clean Code")
-        //            {
-        //                price = 25.5;
-        //                return true;
-        //            }
-        //            else
-        //            {
-        //                price = 0;
-        //                return false;
-        //            }
-        //        }
-        //    }
-        //}
-
-
-        // Q2
-        //Q3
-        //    static void PrintBookInfo(string title, int pages = 300)
-        //{
-        //    Console.WriteLine(title);
-        //    Console.WriteLine(pages);
-        //}
-        //        }
-        //    }
-
-
-
-
-
-        //Q4
-//        static void PrintAllTitles(params string[] titles)
-//        {
-//            foreach (string title in titles)
-//            {
-//                Console.WriteLine(title);
-//            }
+//            Console.WriteLine(book.Password);
 //        }
 //    }
 
+
+
+
+
+
+    //            //Q4
+    //            PrintAllTitles("Clean Code", "C# Basics", "The Pragmatic Programmer");
+    //        }
+
+
+
+
+    //    //Q3
+    //    PrintBookInfo(pages: 500, title: "Clean Code");
+    //}
+
+
+
+
+
+    //                PrintBookInfo("Clean Code");
+
+    //                PrintBookInfo("C# Basics", 400);
+    //            }
+
+
+
+
+
+
+
+    //            double price;
+
+    //            if (TryGetPrice("Clean Code", out price))
+    //            {
+    //                Console.WriteLine(price);
+    //            }
+    //        }
+
+
+
+
+    //        // Q1
+    //        public static bool TryGetPrice(string title, out double price)
+    //        {
+    //            if (title == "Clean Code")
+    //            {
+    //                price = 25.5;
+    //                return true;
+    //            }
+    //            else
+    //            {
+    //                price = 0;
+    //                return false;
+    //            }
+    //        }
+    //    }
+    //}
+
+
+    // Q2
+    //Q3
+    //    static void PrintBookInfo(string title, int pages = 300)
+    //{
+    //    Console.WriteLine(title);
+    //    Console.WriteLine(pages);
+    //}
+    //        }
+    //    }
+
+
+
+
+
+    //Q4
+    //        static void PrintAllTitles(params string[] titles)
+    //        {
+    //            foreach (string title in titles)
+    //            {
+    //                Console.WriteLine(title);
+    //            }
+    //        }
+    //    }
+
+    //}
+
+    //Q5
+//    class Book
+//    {
+//        private string password = "secret";
+//        public string Password => password;
+//    }
 //}
