def25cc208b361d65c5e337d663a32a95e86fcdd
Mariam Omara <mariamomara2005@gmail.com>
2026-09-12 01:29:45 +0300

    Q9

--- C#ASS-05/Program.cs
+++ C#ASS-05/Program.cs
@@ -7,8 +7,20 @@
 //        //            //Q2
 //        static void Main(string[] args)
 //        {
-//            //Q8
-//            Book book = new Book();
+//            //Q9
+
+//            Console.WriteLine((int)Genre.Fiction);
+//            Console.WriteLine((int)Genre.NonFiction);
+//            Console.WriteLine((int)Genre.Science);
+//        }
+
+
+
+
+
+
+        //            //Q8
+        //            Book book = new Book();
 
 //            book.Genre = Genre.Science;
 
@@ -20,153 +32,153 @@
 
 
 
-        //            //Q7
-        //            Book book = new Book();
+//            //Q7
+//            Book book = new Book();
 
-        //            book.Title = "Clean Code";
+//            book.Title = "Clean Code";
 
-        //            Console.WriteLine(book.Title);
-        //        }
-
-
-
-
-
-
-        //Q6
-
-        //            Book book = new Book();
-
-        //            Console.WriteLine(book.copiesInStock);
-        //        }
-
-
-
-
-        //            //Q5
-
-        //            Book book = new Book();
-
-        //            Console.WriteLine(book.Password);
-        //        }
-        //    }
+//            Console.WriteLine(book.Title);
+//        }
 
 
 
 
 
 
-        //            //Q4
-        //            PrintAllTitles("Clean Code", "C# Basics", "The Pragmatic Programmer");
-        //        }
+//Q6
+
+//            Book book = new Book();
+
+//            Console.WriteLine(book.copiesInStock);
+//        }
 
 
 
 
-        //    //Q3
-        //    PrintBookInfo(pages: 500, title: "Clean Code");
-        //}
+//            //Q5
 
+//            Book book = new Book();
 
-
-
-
-        //                PrintBookInfo("Clean Code");
-
-        //                PrintBookInfo("C# Basics", 400);
-        //            }
+//            Console.WriteLine(book.Password);
+//        }
+//    }
 
 
 
 
 
 
-
-        //            double price;
-
-        //            if (TryGetPrice("Clean Code", out price))
-        //            {
-        //                Console.WriteLine(price);
-        //            }
-        //        }
+//            //Q4
+//            PrintAllTitles("Clean Code", "C# Basics", "The Pragmatic Programmer");
+//        }
 
 
 
 
-        //        // Q1
-        //        public static bool TryGetPrice(string title, out double price)
-        //        {
-        //            if (title == "Clean Code")
-        //            {
-        //                price = 25.5;
-        //                return true;
-        //            }
-        //            else
-        //            {
-        //                price = 0;
-        //                return false;
-        //            }
-        //        }
-        //    }
-        //}
-
-
-        // Q2
-        //Q3
-        //    static void PrintBookInfo(string title, int pages = 300)
-        //{
-        //    Console.WriteLine(title);
-        //    Console.WriteLine(pages);
-        //}
-        //        }
-        //    }
+//    //Q3
+//    PrintBookInfo(pages: 500, title: "Clean Code");
+//}
 
 
 
 
 
-        //Q4
-        //        static void PrintAllTitles(params string[] titles)
-        //        {
-        //            foreach (string title in titles)
-        //            {
-        //                Console.WriteLine(title);
-        //            }
-        //        }
-        //    }
+//                PrintBookInfo("Clean Code");
 
-        //}
-
-        //Q5
-        //    class Book
-        //    {
-        //        private string password = "secret";
-        //        public string Password => password;
-        //    }
-        //}
-
-
-        //Q6
-        //        class Book
-        //        {
-        //            internal int copiesInStock = 5;
-        //        }
-        //    }
-        //}
-
-
-        //Q7
-        //            class Book
-        //        {
-        //            public string Title;
-        //        }
-        //    }
-        //}
+//                PrintBookInfo("C# Basics", 400);
+//            }
 
 
 
-        //Q8
 
+
+
+
+//            double price;
+
+//            if (TryGetPrice("Clean Code", out price))
+//            {
+//                Console.WriteLine(price);
+//            }
+//        }
+
+
+
+
+//        // Q1
+//        public static bool TryGetPrice(string title, out double price)
+//        {
+//            if (title == "Clean Code")
+//            {
+//                price = 25.5;
+//                return true;
+//            }
+//            else
+//            {
+//                price = 0;
+//                return false;
+//            }
+//        }
+//    }
+//}
+
+
+// Q2
+//Q3
+//    static void PrintBookInfo(string title, int pages = 300)
+//{
+//    Console.WriteLine(title);
+//    Console.WriteLine(pages);
+//}
+//        }
+//    }
+
+
+
+
+
+//Q4
+//        static void PrintAllTitles(params string[] titles)
+//        {
+//            foreach (string title in titles)
+//            {
+//                Console.WriteLine(title);
+//            }
+//        }
+//    }
+
+//}
+
+//Q5
+//    class Book
+//    {
+//        private string password = "secret";
+//        public string Password => password;
+//    }
+//}
+
+
+//Q6
+//        class Book
+//        {
+//            internal int copiesInStock = 5;
+//        }
+//    }
+//}
+
+
+//Q7
+//            class Book
+//        {
+//            public string Title;
+//        }
+//    }
+//}
+
+
+
+//Q8
+//Q9
 //        enum Genre
 //        {
 //            Fiction,
