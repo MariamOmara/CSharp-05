bbddd494a679d65579c1c7c0a3280e528419b0ab
Mariam Omara <mariamomara2005@gmail.com>
2026-09-12 00:58:38 +0300

    Q1

--- C#ASS-05/Program.cs
+++ C#ASS-05/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace C_ASS_05
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//namespace C_ASS_05
+//{
+//    internal class Program
+//    {
+
+
+//        static void Main(string[] args)
+//        {
+//            double price;
+
+//            if (TryGetPrice("Clean Code", out price))
+//            {
+//                Console.WriteLine(price);
+//            }
+//        }
+
+
+
+
+//        // Q1
+//        public static bool TryGetPrice(string title, out double price)
+//        {
+//            if (title == "Clean Code")
+//            {
+//                price = 25.5;
+//                return true;
+//            }
+//            else
+//            {
+//                price = 0;
+//                return false;
+//            }
+//        }
+//    }
+//}
