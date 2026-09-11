4ec596db1e0c45f74d523b020ef85eb49ab20623
Mariam Omara <mariamomara2005@gmail.com>
2026-09-12 01:31:30 +0300

    Q10

--- C#ASS-05/Program.cs
+++ C#ASS-05/Program.cs
@@ -7,12 +7,25 @@
 //        //            //Q2
 //        static void Main(string[] args)
 //        {
-//            //Q9
 
-//            Console.WriteLine((int)Genre.Fiction);
-//            Console.WriteLine((int)Genre.NonFiction);
-//            Console.WriteLine((int)Genre.Science);
+//            //Q10
+
+//            int genreNumber = 1;
+
+//            Genre genre = (Genre)genreNumber;
+
+//            Console.WriteLine(genre);
 //        }
+
+
+
+
+        //            //Q9
+
+        //            Console.WriteLine((int)Genre.Fiction);
+        //            Console.WriteLine((int)Genre.NonFiction);
+        //            Console.WriteLine((int)Genre.Science);
+        //        }
 
 
 
@@ -22,71 +35,71 @@
         //            //Q8
         //            Book book = new Book();
 
-//            book.Genre = Genre.Science;
+        //            book.Genre = Genre.Science;
 
-//            Console.WriteLine(book.Genre);
-//        }
+        //            Console.WriteLine(book.Genre);
+        //        }
 
 
 
 
 
 
-//            //Q7
-//            Book book = new Book();
+        //            //Q7
+        //            Book book = new Book();
 
-//            book.Title = "Clean Code";
+        //            book.Title = "Clean Code";
 
-//            Console.WriteLine(book.Title);
-//        }
+        //            Console.WriteLine(book.Title);
+        //        }
 
 
 
 
 
 
-//Q6
+        //Q6
 
-//            Book book = new Book();
+        //            Book book = new Book();
 
-//            Console.WriteLine(book.copiesInStock);
-//        }
+        //            Console.WriteLine(book.copiesInStock);
+        //        }
 
 
 
 
-//            //Q5
+        //            //Q5
 
-//            Book book = new Book();
+        //            Book book = new Book();
 
-//            Console.WriteLine(book.Password);
-//        }
-//    }
+        //            Console.WriteLine(book.Password);
+        //        }
+        //    }
 
 
 
 
 
 
-//            //Q4
-//            PrintAllTitles("Clean Code", "C# Basics", "The Pragmatic Programmer");
-//        }
+        //            //Q4
+        //            PrintAllTitles("Clean Code", "C# Basics", "The Pragmatic Programmer");
+        //        }
 
 
 
 
-//    //Q3
-//    PrintBookInfo(pages: 500, title: "Clean Code");
-//}
+        //    //Q3
+        //    PrintBookInfo(pages: 500, title: "Clean Code");
+        //}
 
 
 
 
 
-//                PrintBookInfo("Clean Code");
+        //                PrintBookInfo("Clean Code");
 
-//                PrintBookInfo("C# Basics", 400);
-//            }
+        //                PrintBookInfo("C# Basics", 400);
+        //            }
 
 
 
@@ -94,91 +107,92 @@
 
 
 
-//            double price;
+        //            double price;
 
-//            if (TryGetPrice("Clean Code", out price))
-//            {
-//                Console.WriteLine(price);
-//            }
-//        }
+        //            if (TryGetPrice("Clean Code", out price))
+        //            {
+        //                Console.WriteLine(price);
+        //            }
+        //        }
 
 
 
 
-//        // Q1
-//        public static bool TryGetPrice(string title, out double price)
-//        {
-//            if (title == "Clean Code")
-//            {
-//                price = 25.5;
-//                return true;
-//            }
-//            else
-//            {
-//                price = 0;
-//                return false;
-//            }
-//        }
-//    }
-//}
+        //        // Q1
+        //        public static bool TryGetPrice(string title, out double price)
+        //        {
+        //            if (title == "Clean Code")
+        //            {
+        //                price = 25.5;
+        //                return true;
+        //            }
+        //            else
+        //            {
+        //                price = 0;
+        //                return false;
+        //            }
+        //        }
+        //    }
+        //}
 
 
-// Q2
-//Q3
-//    static void PrintBookInfo(string title, int pages = 300)
-//{
-//    Console.WriteLine(title);
-//    Console.WriteLine(pages);
-//}
-//        }
-//    }
+        // Q2
+        //Q3
+        //    static void PrintBookInfo(string title, int pages = 300)
+        //{
+        //    Console.WriteLine(title);
+        //    Console.WriteLine(pages);
+        //}
+        //        }
+        //    }
 
 
 
 
 
-//Q4
-//        static void PrintAllTitles(params string[] titles)
-//        {
-//            foreach (string title in titles)
-//            {
-//                Console.WriteLine(title);
-//            }
-//        }
-//    }
+        //Q4
+        //        static void PrintAllTitles(params string[] titles)
+        //        {
+        //            foreach (string title in titles)
+        //            {
+        //                Console.WriteLine(title);
+        //            }
+        //        }
+        //    }
 
-//}
+        //}
 
-//Q5
-//    class Book
-//    {
-//        private string password = "secret";
-//        public string Password => password;
-//    }
-//}
+        //Q5
+        //    class Book
+        //    {
+        //        private string password = "secret";
+        //        public string Password => password;
+        //    }
+        //}
 
 
-//Q6
-//        class Book
-//        {
-//            internal int copiesInStock = 5;
-//        }
-//    }
-//}
+        //Q6
+        //        class Book
+        //        {
+        //            internal int copiesInStock = 5;
+        //        }
+        //    }
+        //}
 
 
-//Q7
-//            class Book
-//        {
-//            public string Title;
-//        }
-//    }
-//}
+        //Q7
+        //            class Book
+        //        {
+        //            public string Title;
+        //        }
+        //    }
+        //}
 
 
 
-//Q8
-//Q9
+        //Q8
+        //Q9
+        //Q10
 //        enum Genre
 //        {
 //            Fiction,
